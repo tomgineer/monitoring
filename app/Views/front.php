d82b8b8bbdf56d54d831b4b1058bf1b6a65fb733
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2026-01-18 10:29:02 +0100

    New Favicon

--- app/Views/front.php
+++ app/Views/front.php
@@ -11,7 +11,12 @@
     <link rel="stylesheet" href="<?= base_url('css/tailwind.css') ?>?v=<?=MONITORING_VERSION?>">
     <link rel="stylesheet" href="<?= base_url('assets/fonts/fonts.css') ?>?v=<?=MONITORING_VERSION?>">
     <script src="<?= base_url('js/monitoring-dist.js') ?>?v=<?=MONITORING_VERSION?>" defer></script>
-    <link rel="icon" type="image/svg+xml" href="<?= base_url('gfx/favicon.svg') ?>">
+
+    <link rel="icon" type="image/svg+xml" href="<?=base_url('gfx/favicon/favicon.svg')?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('gfx/favicon/favicon-32.png')?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('gfx/favicon/apple-touch-icon.png')?>">
+    <link rel="icon" href="<?=base_url('gfx/favicon/favicon.ico')?>">
+
 </head>
 
 <body class="min-h-screen flex flex-col bg-glow">
